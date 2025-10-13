4aa9993d3f28cfafb864bd68102269f58f5eae45
Joesiah <tsunetoishida03@gmail.com>
2025-10-13 19:32:56 +1100

    Added comments for js ffile

--- js/vega_embed.js
+++ js/vega_embed.js
@@ -1,14 +1,17 @@
+// Sets up default vis for world map on load
 document.addEventListener("DOMContentLoaded", function() {
   let map_spec = "js/map_chloropleth_anime_popularity_2025.vg.json";
   vegaEmbed("#world-map", map_spec, {mode: "vega-lite"});
 });
 
+// Map filter logic
 document.getElementById("map-selection").addEventListener("change", function() {
     const year = this.value;
     let map_spec = `js/map_chloropleth_anime_popularity_${year}.vg.json`
     vegaEmbed("#world-map", map_spec, {mode: "vega-lite"}).then(console.log).catch(console.warn);
 });
 
+// Trends graph setup
 document.addEventListener("DOMContentLoaded", function() {
   let trends_spec = "js/line-chart-anime-trends.vg.json";
   vegaEmbed("#trends-chart", trends_spec, {mode: "vega-lite"});
